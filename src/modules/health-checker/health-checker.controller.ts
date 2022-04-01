77cb7dc3b4ad1cce55d9df0a19b5dd0ceb7b225c
Sunwook KIm <sunus7@naver.com>
2022-04-02 00:44:38 +0900

    Update health-checker.controller.ts

--- src/modules/health-checker/health-checker.controller.ts
+++ src/modules/health-checker/health-checker.controller.ts
@@ -1,14 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import type { HealthCheckResult } from '@nestjs/terminus';
-import {
-  HealthCheck,
-  HealthCheckService,
-  TypeOrmHealthIndicator,
-} from '@nestjs/terminus';
+import { HealthCheck, HealthCheckService, TypeOrmHealthIndicator } from '@nestjs/terminus';
 
+import { ApiTag } from '../../constants/api-tag';
 import { ServiceHealthIndicator } from './health-indicators/service.indicator';
 
 @Controller('health')
+@ApiTags(ApiTag.DEFAULT)
 export class HealthCheckerController {
   constructor(
     private healthCheckService: HealthCheckService,
@@ -18,6 +17,7 @@ export class HealthCheckerController {
 
   @Get()
   @HealthCheck()
+  @ApiOkResponse()
   async check(): Promise<HealthCheckResult> {
     return this.healthCheckService.check([
       () => this.ormIndicator.pingCheck('database', { timeout: 1500 }),
