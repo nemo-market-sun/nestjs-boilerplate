9757aff5b396195635f8c49487ba8d51e83a5d0d
Sunwook KIm <sunus7@naver.com>
2022-04-02 00:49:10 +0900

    Update user.dto.ts

--- src/modules/user/dtos/user.dto.ts
+++ src/modules/user/dtos/user.dto.ts
@@ -1,34 +1,45 @@
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { Allow } from 'class-validator';
 
 import { AbstractDto } from '../../../common/dto/abstract.dto';
 import { RoleType } from '../../../constants';
+import { IsPhoneNumber } from '../../../decorators';
 import type { UserEntity } from '../user.entity';
 
 // TODO, remove this class and use constructor's second argument's type
 export type UserDtoOptions = Partial<{ isActive: boolean }>;
 
 export class UserDto extends AbstractDto {
+  @Allow()
   @ApiPropertyOptional()
   firstName?: string;
 
+  @Allow()
   @ApiPropertyOptional()
   lastName?: string;
 
+  @Allow()
   @ApiProperty()
   username: string;
 
-  @ApiPropertyOptional({ enum: RoleType })
-  role: RoleType;
+  @Allow()
+  @ApiPropertyOptional({ enum: RoleType, enumName: 'RoleType' })
+  role?: RoleType;
 
+  @Allow()
   @ApiPropertyOptional()
   email?: string;
 
+  @Allow()
   @ApiPropertyOptional()
   avatar?: string;
 
+  @Allow()
   @ApiPropertyOptional()
+  @IsPhoneNumber({ region: 'KR' })
   phone?: string;
 
+  @Allow()
   @ApiPropertyOptional()
   isActive?: boolean;
 
