55bf66c933ed9077498278233ee989e0ca139841
Sunwook KIm <sunus7@naver.com>
2022-04-02 00:41:03 +0900

    Update user-subscriber.ts

--- src/entity-subscribers/user-subscriber.ts
+++ src/entity-subscribers/user-subscriber.ts
@@ -1,15 +1,16 @@
-import type {
-  EntitySubscriberInterface,
-  InsertEvent,
-  UpdateEvent,
-} from 'typeorm';
+import { CommandBus } from '@nestjs/cqrs';
+import type { EntitySubscriberInterface, InsertEvent, UpdateEvent } from 'typeorm';
 import { EventSubscriber } from 'typeorm';
 
-import { generateHash } from '../common/utils';
+import { SetIsPasswordHashedCommand } from '../modules/user/commands/set-is-password-hashed.command';
 import { UserEntity } from '../modules/user/user.entity';
+import type { UserSettingsEntity } from '../modules/user/user-settings.entity';
+import { generateHash } from '../utils/hash';
 
 @EventSubscriber()
 export class UserSubscriber implements EntitySubscriberInterface<UserEntity> {
+  constructor(private commandBus: CommandBus) {}
+
   listenTo(): typeof UserEntity {
     return UserEntity;
   }
@@ -20,12 +21,16 @@ export class UserSubscriber implements EntitySubscriberInterface<UserEntity> {
     }
   }
 
-  beforeUpdate(event: UpdateEvent<UserEntity>): void {
+  async beforeUpdate(event: UpdateEvent<UserEntity>): Promise<void> {
     // FIXME check event.databaseEntity.password
     const entity = event.entity as UserEntity;
 
     if (entity.password !== event.databaseEntity.password) {
-      entity.password = generateHash(entity.password!);
+      entity.password = generateHash(entity.password);
+
+      await this.commandBus.execute<SetIsPasswordHashedCommand, UserSettingsEntity>(
+        new SetIsPasswordHashedCommand(entity.id),
+      );
     }
   }
 }
